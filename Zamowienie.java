fbe50b80ad0d84d0b32ec67fe4bcddd12c3659f0
byczeczku <s33867@pjwstk.edu.pl>
2025-03-20 11:15:35 +0100

    dodałem gettery i settery

--- Zamowienie.java
+++ Zamowienie.java
@@ -6,4 +6,42 @@ public class Zamowienie
     private int[] ilosci;
     private String dataZamowienia;
     private String status;
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getKlient() {
+        return klient;
+    }
+    public void setKlient(String klient) {
+        this.klient = klient;
+    }
+    public Produkt[] getProdukty() {
+        return Produkty;
+    }
+    public void setProdukty(Produkt[] produkty) {
+        Produkty = produkty;
+    }
+    public int[] getIlosci() {
+        return ilosci;
+    }
+    public void setIlosci(int[] ilosci) {
+        this.ilosci = ilosci;
+    }
+    public String getDataZamowienia() {
+        return dataZamowienia;
+    }
+    public void setDataZamowienia(String dataZamowienia) {
+        this.dataZamowienia = dataZamowienia;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
